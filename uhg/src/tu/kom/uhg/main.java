4baef8f4ed3f37741f0a31bc40ae927351207c7d
Kroshka <kit0001@gmail.com>
2013-07-26 21:02:31 +0200

    initial uhg

--- uhg/src/tu/kom/uhg/main.java
+++ uhg/src/tu/kom/uhg/main.java
@@ -11,5 +11,6 @@ public class main extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        //COMMENT
     }
 }
