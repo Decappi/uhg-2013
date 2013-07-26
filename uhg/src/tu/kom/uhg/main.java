d38accdd59467cb7d6ed9153e36ed6791fa86a5f
Kroshka <kit0001@gmail.com>
2013-07-26 20:44:20 +0200

    initial uhg

---
+++ uhg/src/tu/kom/uhg/main.java
@@ -0,0 +1,15 @@
+package tu.kom.uhg;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class main extends Activity {
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
