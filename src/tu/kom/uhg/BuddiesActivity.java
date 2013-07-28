85dded7654390004c3da20e6ae53d8a40a558180
Kroshka <kit0001@gmail.com>
2013-07-28 20:40:50 +0200

    message

--- src/tu/kom/uhg/BuddiesActivity.java
+++ src/tu/kom/uhg/BuddiesActivity.java
@@ -1,15 +1,18 @@
 package tu.kom.uhg;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.Toast;
 
 public class BuddiesActivity extends GenericActivity{
 	
-	ListView myList;
-    String[] listContent = {
+	ListView offlineList;
+	ListView onlineList;
+    String[] defaultBuddyList = {
     		"Gordon Freeman",
     		"Agent Smith",
     		"Jackie Chan"
@@ -20,8 +23,26 @@ public class BuddiesActivity extends GenericActivity{
 	    setContentView(R.layout.activity_buddies);
 	    Toast.makeText(this, "buddies activity loaded", Toast.LENGTH_LONG).show();
 	
+	    SharedPreferences prefs = getSharedPreferences("buddies", MODE_PRIVATE);
+	    int buddiesNum = prefs.getInt("buddies_num", 3);
+	    buddiesNum = buddiesNum-1;
+	    String[] buddies = new String[buddiesNum];
+	    for (int i = 1; i < buddiesNum; i++){
+	    	buddies[i-1] = prefs.getString("buddy_"+i+"name", "noname");
+	    }
+	    if (buddies[0].equals("noname"))
+	    	buddies = defaultBuddyList;
 	    
-	
+	    
+	    //add 
+	    ArrayAdapter<String> adapterOffline = new ArrayAdapter<String>(this,
+	    		android.R.layout.simple_list_item_1,
+	    		buddies);
+	    
+	    offlineList = (ListView)findViewById(R.id.listOfflineFriends);
+	    offlineList.setAdapter(adapterOffline);
+	  
+	    
 	
 	}
 	
