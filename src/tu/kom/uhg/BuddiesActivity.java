8f096242109f2035c216891c83d2f2af206f90a9
Kroshka <kit0001@gmail.com>
2013-07-29 00:11:32 +0200

    Working head

--- src/tu/kom/uhg/BuddiesActivity.java
+++ src/tu/kom/uhg/BuddiesActivity.java
@@ -24,19 +24,30 @@ public class BuddiesActivity extends GenericActivity {
 
 		SharedPreferences prefs = getSharedPreferences("buddies", MODE_PRIVATE);
 		int buddiesNum = prefs.getInt("buddies_num", 3);
-		buddiesNum = buddiesNum - 1;
+		//buddiesNum = buddiesNum;
 		String[] buddies = new String[buddiesNum];
-		for (int i = 1; i < buddiesNum; i++) {
-			buddies[i - 1] = prefs.getString("buddy_" + i + "_name", "noname");
+		for (int i = 1; i <= buddiesNum; i++) {
+			buddies[i - 1] = prefs.getString("buddyname_" + i, "noname");
 		}
-		if (buddies[0].equals("noname"))
+		if (buddies[0].equals("noname")){
 			buddies = defaultBuddyList;
-
+			for(int i = 0; i < defaultBuddyList.length; i++){
+				prefs.edit()
+				.putString("buddyname_" + (i+1), defaultBuddyList[i])
+				.putInt("buddies_num", (i+1))
+				.commit();
+			}
+			prefs = getSharedPreferences("buddies", MODE_PRIVATE);
+			for (int i = 1; i <= buddiesNum; i++) {
+				buddies[i - 1] = prefs.getString("buddyname_" + i, "noname");
+			}
+		}
+			
 		// add
 		ArrayAdapter<String> adapterOffline = new ArrayAdapter<String>(this,
 				android.R.layout.simple_list_item_1, buddies);
 
-		offlineList = (ListView) findViewById(R.id.listOfflineBuddies);
+		offlineList = (ListView)findViewById(R.id.listOfflineBuddies);
 		offlineList.setAdapter(adapterOffline);
 
 	}
@@ -57,24 +68,28 @@ public class BuddiesActivity extends GenericActivity {
 		SharedPreferences prefs = getSharedPreferences("buddies", MODE_PRIVATE);
 		int buddiesNum = prefs.getInt("buddies_num", 3);
 		prefs.edit()
-			.putString("buddy_"+ buddiesNum+1 +"_name", newBuddyName)
-			.putInt("buddies_num", buddiesNum+1)
+			.putString("buddyname_"+ (buddiesNum+1), newBuddyName)
+			.putInt("buddies_num", (buddiesNum+1))
 			.commit();
+		prefs = getSharedPreferences("buddies", MODE_PRIVATE);
 		//update the listview
 		offlineList = (ListView) findViewById(R.id.listOfflineBuddies);
-		
-		String[] buddies = new String[buddiesNum];
-		for (int i = 1; i < buddiesNum; i++) {
-			buddies[i - 1] = prefs.getString("buddy_" + i + "_name", "noname");
+		String[] buddies = new String[buddiesNum + 1];
+		for (int i = 1; i < buddiesNum+1; i++) {
+			buddies[i - 1] = prefs.getString("buddyname_" + i, "noname");
 		}
+		
 		if (buddies[0].equals("noname"))
-			buddies = defaultBuddyList;
+			for(int i = 0; i < defaultBuddyList.length; i++){
+				buddies[i] = defaultBuddyList[i];
+			}
+		//add to buddies newBuddyName
+		buddies[buddiesNum] = newBuddyName;
+		
 		
 		ArrayAdapter<String> adapterOffline = new ArrayAdapter<String>(this,
 				android.R.layout.simple_list_item_1, buddies);
-		//offlineList.setAdapter(adapterOffline);
 		//adapterOffline.add(newBuddyName);
-		//adapterOffline.add("test name");
 		offlineList.setAdapter(adapterOffline);
 	}
 
