a7f03396c4bbce1b63257141d5f459bed04d7161
ph6 <evilsant@hotmail.com>
2013-09-03 21:09:27 +0200

    parkour A added markers

--- src/tu/kom/uhg/MapActivity.java
+++ src/tu/kom/uhg/MapActivity.java
@@ -9,7 +9,9 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.location.Criteria;
 import android.location.Location;
@@ -31,27 +33,46 @@ OnMarkerClickListener, android.location.LocationListener{
 	private static final LatLng MARKER5 = new LatLng(49.8773, 8.65615);
 	
 	//Trimm-dich-Pfad A (11 Stages)
-	/*
-	private static final LatLng STAGEA1 = new LatLng(49.8513, 8.63194);
-	private static final LatLng STAGEA2 = new LatLng(49.8513, 8.63194);
-	private static final LatLng STAGEA3 = new LatLng(49.8513, 8.63194);
-	private static final LatLng STAGEA4 = new LatLng(49.8513, 8.63194);
-	private static final LatLng STAGEA5 = new LatLng(49.8513, 8.63194);
-	private static final LatLng STAGEA6 = new LatLng(49.8513, 8.63194);
-	private static final LatLng STAGEA7 = new LatLng(49.8513, 8.63194);
-	private static final LatLng STAGEA8 = new LatLng(49.8513, 8.63194);
-	private static final LatLng STAGEA9 = new LatLng(49.8513, 8.63194);
-	private static final LatLng STAGEA10 = new LatLng(49.8513, 8.63194);
-	private static final LatLng STAGEA11 = new LatLng(49.8513, 8.63194);
-	*/
+	private static final LatLng PARKOURA = new LatLng(49.8762, 8.65472);
+	private static final LatLng STAGEA1 = new LatLng(49.8761, 8.65464);
+	private static final LatLng STAGEA2 = new LatLng(49.877, 8.65383);
+	private static final LatLng STAGEA3 = new LatLng(49.8795, 8.65326);
+	private static final LatLng STAGEA4 = new LatLng(49.8794, 8.653);
+	private static final LatLng STAGEA5 = new LatLng(49.8797, 8.65104);
+	private static final LatLng STAGEA6 = new LatLng(49.8797, 8.65104);
+	private static final LatLng STAGEA7 = new LatLng(49.8796, 8.65092);
+	private static final LatLng STAGEA8 = new LatLng(49.8793, 8.65083);
+	private static final LatLng STAGEA9 = new LatLng(49.8786, 8.65078);
+	private static final LatLng STAGEA10 = new LatLng(49.8782, 8.65073);
+	private static final LatLng STAGEA11 = new LatLng(49.8778, 8.65066);
+	private Marker parkourA;
+	private Marker parkourA1;
+	private Marker parkourA2;
+	private Marker parkourA3;
+	private Marker parkourA4;
+	private Marker parkourA5;
+	private Marker parkourA6;
+	private Marker parkourA7;
+	private Marker parkourA8;
+	private Marker parkourA9;
+	private Marker parkourA10;
+	private Marker parkourA11;
+	
+
 	//Trimm-dich-Pfad B (1 Stage, multiply tasks)
 	//private static final LatLng STAGEB1 = new LatLng(49.8513, 8.63194);
+	
+	//Ruhepunkt A
+	
+	//Ruhepunkt B
 
 	private Marker marker1;
 	private Marker marker2;
 	private Marker marker3;
 	private Marker marker4;
 	private Marker marker5;
+	
+	Context context = this;
 	
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -74,9 +95,24 @@ OnMarkerClickListener, android.location.LocationListener{
 	        System.out.println("Provider " + provider + " has been selected.");
 	        onLocationChanged(location);
 	    }
+	    
+	    addMarkersToMap(location);
 	}
 	
 	private void addMarkersToMap(Location myLoc) {
+		//Parkours
+		//Parkour A
+		Location parkourA_loc = new Location("ParkourA");
+		parkourA_loc.setLatitude(PARKOURA.latitude);
+		parkourA_loc.setLongitude(PARKOURA.longitude);
+		parkourA = map.addMarker(new MarkerOptions()
+		.position(PARKOURA)
+		.title("Parkour A")
+        .icon(BitmapDescriptorFactory.fromResource(R.drawable.activities_big)));
+		
+		
+		
+		
 		Location loc1 = new Location("Marker1");
 		loc1.setLatitude(MARKER1.latitude);
 		loc1.setLongitude(MARKER1.longitude);
@@ -124,9 +160,38 @@ OnMarkerClickListener, android.location.LocationListener{
 	
 	@Override
     public boolean onMarkerClick(final Marker marker) {
-		Intent intent = new Intent(MapActivity.this, QuizActivity.class);
-		intent.putExtra("markerId", marker.getId());
-        startActivity(intent);
+		//Toast.makeText(MapActivity.this, "Marker " + marker.getTitle() + " clicked", Toast.LENGTH_LONG).show();
+		
+		//check markers ID
+		if(marker.getTitle().equals("Parkour A")){
+			final AlertDialog alertDialog;
+			alertDialog = new AlertDialog.Builder(MapActivity.this).create();
+			alertDialog.setTitle("Trimm-dich-Pfad");
+			alertDialog.setMessage("Der Halt-Dich-Fit Parkour bietet mit seinen 11 Stationen " +
+					"verteilt über den Herrngarten den idealen Ausgleich zum Büroalltag. " +
+					"Mit kleinen Übungen kannst du deine Fitness täglich verbessern. \nParkour starten?");
+			//ja button
+			alertDialog.setButton(AlertDialog.BUTTON_POSITIVE, "Ja", new DialogInterface.OnClickListener(){
+					
+				public void onClick(DialogInterface dialog, int which) {
+					//start Trimm-dich-Pfad
+					startParkourA();
+				}
+			});
+			//nein button
+			alertDialog.setButton(AlertDialog.BUTTON_POSITIVE, "Nein", new DialogInterface.OnClickListener(){
+
+				public void onClick(DialogInterface dialog, int which){
+					alertDialog.cancel();
+				}
+			});
+			alertDialog.show();
+		}
+		else {
+			Intent intent = new Intent(MapActivity.this, QuizActivity.class);
+			intent.putExtra("markerId", marker.getId());
+	        startActivity(intent);
+		}
         
 		// We return false to indicate that we have not consumed the event and that we wish
         // for the default behavior to occur (which is for the camera to move such that the
@@ -161,7 +226,7 @@ OnMarkerClickListener, android.location.LocationListener{
 		//move camera center to the current position
 		map.animateCamera(CameraUpdateFactory.newLatLng(target));
 		map.clear();
-	    addMarkersToMap(location);
+	    //addMarkersToMap(location);
 	}
 
 	@Override
@@ -176,5 +241,99 @@ OnMarkerClickListener, android.location.LocationListener{
 
 	@Override
 	public void onProviderDisabled(String provider) {
+	}
+	
+	private void startParkourA() {
+		//clear map
+		map.clear();		
+		//show Stages 1-11
+		Location stageA1_loc = new Location("ParkourA1");
+		stageA1_loc.setLatitude(STAGEA1.latitude);
+		stageA1_loc.setLongitude(STAGEA1.longitude);
+		parkourA = map.addMarker(new MarkerOptions()
+		.position(STAGEA1)
+		.title("Station 1")
+        .icon(BitmapDescriptorFactory.fromResource(R.drawable.activities_small)));
+		
+		Location stageA2_loc = new Location("ParkourA2");
+		stageA2_loc.setLatitude(STAGEA2.latitude);
+		stageA2_loc.setLongitude(STAGEA2.longitude);
+		parkourA = map.addMarker(new MarkerOptions()
+		.position(STAGEA2)
+		.title("Station 2")
+        .icon(BitmapDescriptorFactory.fromResource(R.drawable.activities_small)));
+		
+		Location stageA3_loc = new Location("ParkourA3");
+		stageA3_loc.setLatitude(STAGEA3.latitude);
+		stageA3_loc.setLongitude(STAGEA3.longitude);
+		parkourA = map.addMarker(new MarkerOptions()
+		.position(STAGEA3)
+		.title("Station 3")
+        .icon(BitmapDescriptorFactory.fromResource(R.drawable.activities_small)));
+		
+		Location stageA4_loc = new Location("ParkourA4");
+		stageA4_loc.setLatitude(STAGEA4.latitude);
+		stageA4_loc.setLongitude(STAGEA4.longitude);
+		parkourA = map.addMarker(new MarkerOptions()
+		.position(STAGEA4)
+		.title("Station 4")
+        .icon(BitmapDescriptorFactory.fromResource(R.drawable.activities_small)));
+		
+		Location stageA5_loc = new Location("ParkourA5");
+		stageA5_loc.setLatitude(STAGEA5.latitude);
+		stageA5_loc.setLongitude(STAGEA5.longitude);
+		parkourA = map.addMarker(new MarkerOptions()
+		.position(STAGEA5)
+		.title("Station 5")
+        .icon(BitmapDescriptorFactory.fromResource(R.drawable.activities_small)));
+		
+		Location stageA6_loc = new Location("ParkourA6");
+		stageA6_loc.setLatitude(STAGEA6.latitude);
+		stageA6_loc.setLongitude(STAGEA6.longitude);
+		parkourA = map.addMarker(new MarkerOptions()
+		.position(STAGEA6)
+		.title("Station 6")
+        .icon(BitmapDescriptorFactory.fromResource(R.drawable.activities_small)));
+		
+		Location stageA7_loc = new Location("ParkourA7");
+		stageA7_loc.setLatitude(STAGEA7.latitude);
+		stageA7_loc.setLongitude(STAGEA7.longitude);
+		parkourA = map.addMarker(new MarkerOptions()
+		.position(STAGEA7)
+		.title("Station 7")
+        .icon(BitmapDescriptorFactory.fromResource(R.drawable.activities_small)));
+		
+		Location stageA8_loc = new Location("ParkourA8");
+		stageA8_loc.setLatitude(STAGEA8.latitude);
+		stageA8_loc.setLongitude(STAGEA8.longitude);
+		parkourA = map.addMarker(new MarkerOptions()
+		.position(STAGEA8)
+		.title("Station 8")
+        .icon(BitmapDescriptorFactory.fromResource(R.drawable.activities_small)));
+		
+		Location stageA9_loc = new Location("ParkourA9");
+		stageA9_loc.setLatitude(STAGEA9.latitude);
+		stageA9_loc.setLongitude(STAGEA9.longitude);
+		parkourA = map.addMarker(new MarkerOptions()
+		.position(STAGEA9)
+		.title("Station 9")
+        .icon(BitmapDescriptorFactory.fromResource(R.drawable.activities_small)));
+		
+		Location stageA10_loc = new Location("ParkourA10");
+		stageA10_loc.setLatitude(STAGEA10.latitude);
+		stageA10_loc.setLongitude(STAGEA10.longitude);
+		parkourA = map.addMarker(new MarkerOptions()
+		.position(STAGEA10)
+		.title("Station 10")
+        .icon(BitmapDescriptorFactory.fromResource(R.drawable.activities_small)));
+		
+		Location stageA11_loc = new Location("ParkourA11");
+		stageA11_loc.setLatitude(STAGEA11.latitude);
+		stageA11_loc.setLongitude(STAGEA11.longitude);
+		parkourA = map.addMarker(new MarkerOptions()
+		.position(STAGEA11)
+		.title("Station 11")
+        .icon(BitmapDescriptorFactory.fromResource(R.drawable.activities_small)));
+		
 	}
 }
